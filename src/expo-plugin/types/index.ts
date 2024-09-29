4193a01502c41d925315b85e6571a26d716b16bb
Maximilian Krause <hello@max-krause.com>
2024-09-29 14:27:05 +0200

    feat(expo): allow customizing iCloud container identifier

--- src/expo-plugin/types/index.ts
+++ src/expo-plugin/types/index.ts
@@ -3,4 +3,8 @@ export interface RNCloudStorageConfigPluginOptions {
    * The iCloud container environment to use. Defaults to 'Production'.
    */
   iCloudContainerEnvironment?: 'Production' | 'Development';
+  /**
+   * The iCloud container identifier to use. Defaults to `iCloud.{appBundleIdentifier}`
+   */
+  iCloudContainerIdentifier?: string;
 }
--- src/expo-plugin/withRNCloudStorageIos.ts
+++ src/expo-plugin/withRNCloudStorageIos.ts
@@ -1,14 +1,19 @@
 import { withEntitlementsPlist, withInfoPlist, withPlugins, type ConfigPlugin } from '@expo/config-plugins';
+import { type ExpoConfig } from '@expo/config-types';
 import type { RNCloudStorageConfigPluginOptions } from './types';
 
-const withRNCloudStorageInfoPlist: ConfigPlugin = (config) =>
+const getICloudContainerIdentifier = (config: ExpoConfig, options: RNCloudStorageConfigPluginOptions) => {
+  if (options.iCloudContainerIdentifier) return options.iCloudContainerIdentifier;
+
+  if (!config?.ios?.bundleIdentifier) throw new Error('Missing iOS bundle identifier');
+  return `iCloud.${config.ios.bundleIdentifier}`;
+};
+
+const withRNCloudStorageInfoPlist: ConfigPlugin<RNCloudStorageConfigPluginOptions> = (config, options) =>
   withInfoPlist(config, async (newConfig) => {
-    if (!config.ios?.bundleIdentifier) {
-      throw new Error('Missing iOS bundle identifier');
-    }
     const infoPlist = newConfig.modResults;
     infoPlist.NSUbiquitousContainers = {
-      [`iCloud.${config.ios.bundleIdentifier}`]: {
+      [getICloudContainerIdentifier(config, options)]: {
         NSUbiquitousContainerIsDocumentScopePublic: true,
         NSUbiquitousContainerSupportedFolderLevels: 'Any',
         NSUbiquitousContainerName: config.slug,
@@ -24,19 +29,23 @@ const withRNCloudStorageEntitlementsPlist: ConfigPlugin<RNCloudStorageConfigPlug
       throw new Error('Missing iOS bundle identifier');
     }
     const entitlementsPlist = newConfig.modResults;
-    entitlementsPlist['com.apple.developer.icloud-container-identifiers'] = [`iCloud.${config.ios.bundleIdentifier}`];
+    entitlementsPlist['com.apple.developer.icloud-container-identifiers'] = [
+      getICloudContainerIdentifier(config, options),
+    ];
     entitlementsPlist['com.apple.developer.icloud-services'] = ['CloudDocuments'];
     entitlementsPlist['com.apple.developer.icloud-container-environment'] =
       options?.iCloudContainerEnvironment ?? 'Production';
-    entitlementsPlist['com.apple.developer.ubiquity-container-identifiers'] = [`iCloud.${config.ios.bundleIdentifier}`];
-    entitlementsPlist[
-      'com.apple.developer.ubiquity-kvstore-identifier'
-    ] = `$(TeamIdentifierPrefix)${config.ios.bundleIdentifier}`;
+    entitlementsPlist['com.apple.developer.ubiquity-container-identifiers'] = [
+      getICloudContainerIdentifier(config, options),
+    ];
 
     return newConfig;
   });
 
 const withRNCloudStorageIos: ConfigPlugin<RNCloudStorageConfigPluginOptions> = (config, options) =>
-  withPlugins(config, [withRNCloudStorageInfoPlist, [withRNCloudStorageEntitlementsPlist, options]]);
+  withPlugins(config, [
+    [withRNCloudStorageInfoPlist, options],
+    [withRNCloudStorageEntitlementsPlist, options],
+  ]);
 
 export default withRNCloudStorageIos;
