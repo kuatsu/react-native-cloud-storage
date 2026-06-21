3782095bbae5f10d2a00cf1f6571c2a55f3b52ba
Maximilian Krause <max@kuatsu.de>
2026-06-21 20:48:12 +0200

    fix(docs): minor fixes

--- apps/docs/lib/layout.shared.tsx
+++ apps/docs/lib/layout.shared.tsx
@@ -3,7 +3,7 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 export const gitConfig = {
   user: 'kuatsu',
   repo: 'react-native-cloud-storage',
-  branch: 'main',
+  branch: 'master',
 };
 
 export function baseOptions(): BaseLayoutProps {
