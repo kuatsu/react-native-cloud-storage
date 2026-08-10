475866001b4346d6f90d9e8d974314fa59a3944d
Maximilian Krause <max@kuatsu.de>
2026-08-11 01:46:54 +0200

    chore: rename master branch to main

--- apps/docs/lib/layout.shared.tsx
+++ apps/docs/lib/layout.shared.tsx
@@ -3,7 +3,7 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 export const gitConfig = {
   user: 'kuatsu',
   repo: 'react-native-cloud-storage',
-  branch: 'master',
+  branch: 'main',
 };
 
 export function baseOptions(): BaseLayoutProps {
